f874c4576f7b414d12496b31d551af1486b5fd30
StratoDev89 <danieltestingemail01@gmail.com>
2023-11-14 12:22:45 -0400

    mod song comp styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: '',
+  API_URL:
+    // 'https://backend-app-equipo-musica-culto-cx10rqz44-stratodev89.vercel.app',
+    'https://backend-equipo-musica-culto.vercel.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3002',
+  // API_URL: 'http://localhost:3002',
+  API_URL:
+    // 'https://backend-app-equipo-musica-culto-cx10rqz44-stratodev89.vercel.app',
+    'https://backend-equipo-musica-culto.vercel.app'
 };
